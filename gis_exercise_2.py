f5d69251e1886c2428830818147ff85875ec17c4
SGJJSR <55048118+SGJJSR@users.noreply.github.com>
2020-05-13 13:21:15 +0200

    distinguished between comment and task
    ## = Task & given by the exercise
    # = comment

--- gis_exercise_2.py
+++ gis_exercise_2.py
@@ -4,19 +4,19 @@
 
 ############### Problem 1: Create Polygon from lists of coordinates ############# *********SOLVED***************
 
-# In the first problem you should:
+## In the first problem you should:
 
-# create a Polygon out of the the x and y coordinates that are provided in the create_polygon.py -script.
-# script: 
-# https://github.com/AutoGIS-2017/Exercise-2/blob/master/create_polygon.py
-# insert the polygon into a GeoDataFrame
-# save the Polygon into a Shapefile.
-# plot and save a figure out of the Polygon.
-# The create_polygon.py starter script has all necessary steps listed and also some hints are provided. There are all 
-# together 6 steps that you need to fill to accomplish the problem 1. 
-# starter script:
-# https://github.com/AutoGIS-2017/Exercise-2/blob/master/create_polygon.py
-# Each step that you need to fill is marked with capital P -letter (P1 to P6).
+## create a Polygon out of the the x and y coordinates that are provided in the create_polygon.py -script.
+## script: 
+## https://github.com/AutoGIS-2017/Exercise-2/blob/master/create_polygon.py
+## insert the polygon into a GeoDataFrame
+## save the Polygon into a Shapefile.
+## plot and save a figure out of the Polygon.
+## The create_polygon.py starter script has all necessary steps listed and also some hints are provided. There are all 
+## together 6 steps that you need to fill to accomplish the problem 1. 
+## starter script:
+## https://github.com/AutoGIS-2017/Exercise-2/blob/master/create_polygon.py
+## Each step that you need to fill is marked with capital P -letter (P1 to P6).
 
 import os
 #os.chdir("C:\\.\\Exercise_2")
@@ -25,7 +25,7 @@ import geopandas as gpd
 import shapely
 from shapely.geometry import Polygon, Point, LineString
 
-# X -coordinates 
+## X -coordinates 
 xcoords = [29.99671173095703, 31.58196258544922, 27.738052368164062, 26.50013542175293, 26.652359008789062, 25.921663284301758, 2.90027618408203, 23.257217407226562,
            23.335693359375, 22.87444305419922, 23.08465003967285, 22.565473556518555, 21.452774047851562, 21.66388702392578, 21.065969467163086, 21.67659568786621,
            21.496871948242188, 22.339998245239258, 22.288192749023438, 24.539581298828125, 25.444232940673828, 25.303749084472656, 24.669166564941406, 24.689163208007812,
@@ -34,7 +34,7 @@ xcoords = [29.99671173095703, 31.58196258544922, 27.738052368164062, 26.50013542
            28.4355525970459, 28.817358016967773, 28.459857940673828, 30.028610229492188, 29.075136184692383, 30.13492774963379, 29.818885803222656, 29.640830993652344, 30.57735824584961,
            29.99671173095703]
 
-# Y -coordinates
+## Y -coordinates
 ycoords = [63.748023986816406, 62.90789794921875, 60.511383056640625, 60.44499588012695, 60.646385192871094, 60.243743896484375, 59.806800842285156, 59.91944122314453,
            60.02395248413086, 60.14555358886719, 60.3452033996582, 60.211936950683594, 60.56249237060547, 61.54027557373047, 62.59798049926758, 63.02013397216797,
            63.20353698730469, 63.27652359008789, 63.525691986083984, 64.79915618896484, 64.9533920288086, 65.51513671875, 65.65470886230469, 65.89610290527344, 65.79151916503906,
@@ -87,37 +87,37 @@ coordpairs[0]
 poly
 
 ################## Problem 2: Points to map ####################### ***************SOLVED******************
-# The problem 2 this week continues the process that we started last week, i.e. creating geometric point -objects 
-# and putting them into a map. Here our aim is to plot a set of x and y coordinates that we should read 
-# from a some_posts.csv comma separated file that contains following kind of data:
+## The problem 2 this week continues the process that we started last week, i.e. creating geometric point -objects 
+## and putting them into a map. Here our aim is to plot a set of x and y coordinates that we should read 
+## from a some_posts.csv comma separated file that contains following kind of data:
 
-# lat,lon,timestamp,userid
-# -24.980792492,31.484633302,2015-07-07 03:02,66487960
-# -25.499224667,31.508905612,2015-07-07 03:18,65281761
-# -24.342578456,30.930866066,2015-03-07 03:38,90916112
-# -24.85461393,31.519718439,2015-10-07 05:04,37959089
-# The data has 81379 rows and consists of locations and times of social media posts inside Kruger national park in South Africa:
+## lat,lon,timestamp,userid
+## -24.980792492,31.484633302,2015-07-07 03:02,66487960
+## -25.499224667,31.508905612,2015-07-07 03:18,65281761
+## -24.342578456,30.930866066,2015-03-07 03:38,90916112
+## -24.85461393,31.519718439,2015-10-07 05:04,37959089
+## The data has 81379 rows and consists of locations and times of social media posts inside Kruger national park in South Africa:
 
-# Column	Description
-# lat	y-coordinate of the post
-# lon	x-coordinate of the post
-# timestamp	Time when the post was uploaded
-# userid	userid
-# Note: although the data is based on real social media data, it is heavily anonymized. Userids and timestamps have been randomized, i.e. they do not not match with real ones, also spatial accuracy of the data have been lowered.
+## Column	Description
+## lat	y-coordinate of the post
+## lon	x-coordinate of the post
+## timestamp	Time when the post was uploaded
+## userid	userid
+## Note: although the data is based on real social media data, it is heavily anonymized. Userids and timestamps have been randomized, i.e. they do not not match with real ones, also spatial accuracy of the data have been lowered.
 
-# Download the data (Click on the link ==> CNTRL + S)
-# data:
-# https://raw.githubusercontent.com/AutoGIS-2017/Exercise-2/master/data/some_posts.csv?token=AGWdzhgsQbNFI3lk6a5GxrjguPnmuhwoks5YKVWrwA%3D%3D
+## Download the data (Click on the link ==> CNTRL + S)
+## data:
+## https://raw.githubusercontent.com/AutoGIS-2017/Exercise-2/master/data/some_posts.csv?token=AGWdzhgsQbNFI3lk6a5GxrjguPnmuhwoks5YKVWrwA%3D%3D
 
-# Read the data into memory using Pandas
+## Read the data into memory using Pandas
 import pandas as pd
 import numpy as np
 kruger_park = pd.read_csv('data/some_posts.txt', sep=',')
 
-# Create an empty column called geometry where you will store shapely Point objects
+## Create an empty column called geometry where you will store shapely Point objects
 kruger_park['geometry'] = None
 
-# Iterate over the rows of the DataFrame and insert Point objects into column geometry (you need to use .loc indexer to update the row, see materials
+## Iterate over the rows of the DataFrame and insert Point objects into column geometry (you need to use .loc indexer to update the row, see materials
 from shapely.geometry import Polygon, Point, LineString
 
 # x = lon, y = lat
@@ -129,24 +129,24 @@ from shapely.geometry import Polygon, Point, LineString
 kruger_park['geometry'] = [Point(x, y) for x,y in kruger_park[['lat', 'lon']].values]
 kruger_park
 
-# Convert that DataFrame into a GeoDataFrame, see hints
+## Convert that DataFrame into a GeoDataFrame, see hints
 import fiona
 import geopandas as gpd
 from fiona.crs import from_epsg
 
 
-# Update the CRS for coordinate system as WGS84 (i.e. epsg code: 4326)
+## Update the CRS for coordinate system as WGS84 (i.e. epsg code: 4326)
 
 kruger_park = gpd.GeoDataFrame(kruger_park, geometry='geometry', crs=from_epsg(4326))
 
-# Save the data into a Shapefile called Kruger_posts.shp
+## Save the data into a Shapefile called Kruger_posts.shp
 outfp = r"Kruger_posts.shp"
 kruger_park.to_file(outfp)
 
 # ****************loading data again:
 kruger_park = gpd.read_file("Kruger_posts.shp")
 
-# Create a simple map of those points using a GIS software or using .plot() -funtion in Python. Save it to GitHub as png file.
+## Create a simple map of those points using a GIS software or using .plot() -funtion in Python. Save it to GitHub as png file.
 kruger_park.plot(facecolor='blue')
 
 plt.title("Social_media_in_Kruger_park")
@@ -155,34 +155,34 @@ plt.title("Social_media_in_Kruger_park")
 plt.savefig("Kruger_park.png")
 
 ################ Problem 3: How long distance individuals have travelled? 
-# In this problem the aim is to calculate the distance in meters that the individuals have travelled 
-# according the social media posts (Euclidian distances between points).
+## In this problem the aim is to calculate the distance in meters that the individuals have travelled 
+## according the social media posts (Euclidian distances between points).
 
-# Write your codes into the same file as in previous Problem (2).
+## Write your codes into the same file as in previous Problem (2).
 
-# In your code you should:
+## In your code you should:
 
-# Reproject the data from WGS84 projection into EPSG:32735 -projection which stands for UTM Zone 35S 
-#     (UTM zone for South Africa) to transform the data into metric system.
+## Reproject the data from WGS84 projection into EPSG:32735 -projection which stands for UTM Zone 35S 
+##     (UTM zone for South Africa) to transform the data into metric system.
 kruger_park_meters = gpd.GeoDataFrame(kruger_park, geometry='geometry', crs=from_epsg(32735))
 
-# Group the data by userid 
+## Group the data by userid 
 userid = kruger_park_meters.groupby(['userid'])
 
-# Create an empty GeoDataFrame called movements
+## Create an empty GeoDataFrame called movements
 movements= gpd.GeoDataFrame()
 
 
-# For each user:  ############ ????????????????????
-# sort the rows by timestamp
-# hyperlink @sort: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.sort_values.html
+## For each user:  ############ ????????????????????
+## sort the rows by timestamp
+## hyperlink @sort: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.sort_values.html
 
 # I know I could sort the dataframe by user and timestamp at the same time, like this:
 km = kruger_park_meters
 by_user_and_time = km.sort_values(by=['userid', 'timestamp'])
 groups = by_user_and_time.groupby('userid', sort=True)
 
-# create LineString objects based on the points
+## create LineString objects based on the points
 def calculate_distance(rows):
     if len(rows.geometry)>1:
         return LineString(list(rows.geometry)).length
@@ -195,7 +195,7 @@ print(distance_travelled)
 
 
 
-# add the geometry and the userid into the GeoDataFrame you created in the last step
+## add the geometry and the userid into the GeoDataFrame you created in the last step
 # hyperlink @add: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.append.html
 
 # I merged the grouped and distance_travelled information together
@@ -222,21 +222,21 @@ print(distance_df)
 movements = distance_df.merge(groups, how='inner', on='userid')
 
 
-# Determine the CRS of the movements GeoDataFrame to EPSG:32735 (epsg code: 32735)
+## Determine the CRS of the movements GeoDataFrame to EPSG:32735 (epsg code: 32735)
 movements.crs = "EPSG:32735"
 
-# Calculate the lenghts of the lines into a new column called distance in movements GeoDataFrame.
+## Calculate the lenghts of the lines into a new column called distance in movements GeoDataFrame.
 
 # already done
 
-# Save the movements of into a Shapefile called Some_movements.shp
+## Save the movements of into a Shapefile called Some_movements.shp
 outfp = r"C:\\Users\\stefa\\Documents\\Fortbildung privat\\Python\\GIS\\Data\\some_movements.shp"
 movements.to_file(outfp)
 
-# Questions
-# Write your answers below the questions. You should also print in your code the answers to the questions.
+## Questions
+## Write your answers below the questions. You should also print in your code the answers to the questions.
 
-# What was the shortest distance travelled in meters?
+## What was the shortest distance travelled in meters?
 l = []
 for index, rows in distance_df.iterrows():
     #print(rows)
@@ -251,7 +251,7 @@ print("The smallest distance that is not zero is {0:.9f} meters, covered by the 
       .format(existing_distance.loc[existing_distance['distance'].idxmin(), 'distance'],\
               existing_distance.loc[existing_distance['distance'].idxmin(), 'userid']))
 
-# What was the mean distance travelled in meters?
+## What was the mean distance travelled in meters?
 # no-distance users included:
 print("The average distance of all users is {0:.2f} meters".format(movements['distance'].mean()))
 # excluding no-distance users:
@@ -261,7 +261,7 @@ print("The average distance of all users with distance > 0 is {0:.2f} meters".fo
 # lower than the overall mean. I do not know what went wrong, I have tested the code with simpler tables
 # and it works as expected there
 
-# What was the maximum distance travelled in meters?
+## What was the maximum distance travelled in meters?
 print("The largest distance is {0:.2f} meters, covered by the userid {1}"\
       .format(movements.loc[movements['distance'].idxmax(), 'distance'],\
               movements.loc[movements['distance'].idxmax(), 'userid']))
